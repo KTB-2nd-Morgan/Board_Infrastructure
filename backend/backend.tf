7ebab852f4951401cc6fcb71757d8a575b6bae54
koojun99 <koojun99@naver.com>
2025-03-21 13:30:21 +0900

    👷 Ci: 버킷 정책 수정

--- backend/backend.tf
+++ backend/backend.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.terraform_state.id
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
