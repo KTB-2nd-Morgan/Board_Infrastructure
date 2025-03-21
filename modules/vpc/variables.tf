09f9fe181708d4a6743ba71c859786f819b21771
koojun99 <koojun99@naver.com>
2025-03-21 17:04:39 +0900

    🧱 Infra: Terraform 모듈화

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,34 +1,25 @@
-variable "region" {
-  type = string
-  default = "ap-northeast-2"
-}
-
+# VPC
 variable "vpc_main_cidr" {
   type = string
-  default = "192.168.0.0/16"
 }
 
 variable "subnet_public_1" {
   type = string
-  default = "192.168.10.0/24"
 }
 
 variable "subnet_nat_1" {
   type = string
-  default = "192.168.20.0/24"
 }
 
 variable "subnet_private_1" {
   type = string
-  default = "192.168.30.0/24"
 }
 
+# Security Group
 # variable "server_port" {
 #   type = number
-#   default = 80
 # }
 
 # variable "my_ip" {
 #   type = string
-#   default = "0.0.0.0/0"
 # }
