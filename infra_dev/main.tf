d7e6e788f82b08c02c8dc1c3a9ede70c97f0dc85
koojun99 <koojun99@naver.com>
2025-03-25 10:41:08 +0900

    🧱 Infra: SG 수정

--- infra_dev/main.tf
+++ infra_dev/main.tf
@@ -26,47 +26,24 @@ resource "aws_security_group" "sg_ec2" {
   name   = "sg_ec2"
   ingress = [
     {
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
       description      = ""
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
       self             = false
-    },
+    }
+  ]
 
+  egress = [
     {
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    },
-
-    {
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-    },
-
-    {
-      from_port        = 943
-      to_port          = 943
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      description      = ""
       ipv6_cidr_blocks = []
       prefix_list_ids  = []
       security_groups  = []
