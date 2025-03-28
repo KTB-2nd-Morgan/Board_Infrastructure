2225ad2c4f55b552f4e4c6071fce8ec3b4c446e4
koojun99 <koojun99@naver.com>
2025-03-28 14:40:15 +0900

    🧱 Infra: 권한 분리

--- modules/kinesis/firehose.tf
+++ modules/kinesis/firehose.tf
@@ -15,11 +15,11 @@ resource "aws_iam_role" "firehose_role" {
 }
 
 # Firehose Delivery Stream
-
 resource "aws_cloudwatch_log_group" "firehose_error_logs" {
   name              = "/aws/kinesisfirehose/morgan-log-delivery"
   retention_in_days = 7
 }
+
 resource "aws_kinesis_firehose_delivery_stream" "log_delivery" {
   name        = "morgan-log-delivery"
   destination = "extended_s3"
@@ -27,9 +27,9 @@ resource "aws_kinesis_firehose_delivery_stream" "log_delivery" {
   extended_s3_configuration {
     role_arn           = aws_iam_role.firehose_role.arn
     bucket_arn         = var.s3_bucket_arn
-    buffering_size     = 5  # MB 단위 (1~128 MB)
-    buffering_interval = 60 # 초 단위 (60~900초)
-    compression_format = "GZIP"
+    buffering_size     = 1
+    buffering_interval = 60
+    compression_format = "UNCOMPRESSED"
 
     cloudwatch_logging_options {
       enabled         = true
@@ -38,10 +38,10 @@ resource "aws_kinesis_firehose_delivery_stream" "log_delivery" {
     }
   }
 
-
-
-
-  depends_on = [aws_cloudwatch_log_group.firehose_error_logs, aws_iam_role.firehose_role]
+  depends_on = [
+    aws_cloudwatch_log_group.firehose_error_logs,
+    aws_iam_role.firehose_role
+  ]
 }
 
 # Firehose IAM Policy
@@ -63,13 +63,11 @@ resource "aws_iam_policy" "firehose_policy" {
         Effect = "Allow",
         Action = [
           "logs:PutLogEvents",
-          "logs:GetLogEvents",
+          "logs:CreateLogStream",
           "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams",
-          "logs:PutSubscriptionFilter",
-          "logs:DeleteSubscriptionFilter"
+          "logs:DescribeLogStreams"
         ],
-        Resource = "*"
+        Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/kinesisfirehose/*"
       },
       {
         Effect = "Allow",
@@ -78,27 +76,54 @@ resource "aws_iam_policy" "firehose_policy" {
           "firehose:PutRecordBatch"
         ],
         Resource = aws_kinesis_firehose_delivery_stream.log_delivery.arn
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:PutLogEvents",
-          "logs:CreateLogStream",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
-        ],
-        Resource = "arn:aws:logs:${var.aws_region}:${var.aws_account_id}:log-group:/aws/kinesisfirehose/*"
       }
     ]
   })
 }
 
-# Attach Firehose Policy to Role
 resource "aws_iam_role_policy_attachment" "attach_firehose_policy" {
   role       = aws_iam_role.firehose_role.name
   policy_arn = aws_iam_policy.firehose_policy.arn
 }
 
+# CloudWatch to Firehose IAM Role
+resource "aws_iam_role" "cloudwatch_logs_to_firehose" {
+  name = "cloudwatch-logs-to-firehose"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "logs.${var.aws_region}.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "logs_to_firehose_policy" {
+  name = "logs-to-firehose"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch"
+        ],
+        Resource = aws_kinesis_firehose_delivery_stream.log_delivery.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_logs_to_firehose_policy" {
+  role       = aws_iam_role.cloudwatch_logs_to_firehose.name
+  policy_arn = aws_iam_policy.logs_to_firehose_policy.arn
+}
 
 # Subscription Filter
 resource "aws_cloudwatch_log_subscription_filter" "backend_to_firehose" {
@@ -106,6 +131,10 @@ resource "aws_cloudwatch_log_subscription_filter" "backend_to_firehose" {
   log_group_name  = var.backend_log_group_name
   filter_pattern  = ""
   destination_arn = aws_kinesis_firehose_delivery_stream.log_delivery.arn
-  role_arn        = aws_iam_role.firehose_role.arn
-  depends_on      = [aws_kinesis_firehose_delivery_stream.log_delivery, aws_iam_role_policy_attachment.attach_firehose_policy]
+  role_arn        = aws_iam_role.cloudwatch_logs_to_firehose.arn
+
+  depends_on = [
+    aws_kinesis_firehose_delivery_stream.log_delivery,
+    aws_iam_role_policy_attachment.attach_logs_to_firehose_policy
+  ]
 }
